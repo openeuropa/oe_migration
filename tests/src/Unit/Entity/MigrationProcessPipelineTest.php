be8cb80326438a5faae6681bc2c929560faa0d50
Samuel Solis <ssolisfu@gmail.com>
2020-05-27 14:53:53 +0200

    D8MT-450: Added test for the class MigrationProcessPipeline.

---
+++ tests/src/Unit/Entity/MigrationProcessPipelineTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Drupal\Tests\oe_migration\Unit\Entity;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\oe_migration\Entity\MigrationProcessPipeline;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Entity\EntityTypeManager;
+
+/**
+ * Class to test the MigrationProcessPipeline methods.
+ */
+class MigrationProcessPipelineTest extends UnitTestCase {
+
+  /**
+   * The entity type used for testing.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $entityType;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $provider = $this->randomMachineName();
+    $this->entityType = $this->createMock(EntityTypeInterface::class);
+    $this->entityType->expects($this->any())
+      ->method('getProvider')
+      ->willReturn($provider);
+  }
+
+  /**
+   * Test the getProcess method without any replacement nor plugin configured.
+   */
+  public function testGetProcessWithoutPlugins() {
+    $fixed_source = $this->randomMachineName();
+    $values = ['process' => [$fixed_source]];
+
+    // The default plugin should be 'get'.
+    $expected = [
+      [
+        'plugin' => 'get',
+        'source' => $fixed_source,
+      ],
+    ];
+    $pipeline = new MigrationProcessPipeline($values, $this->entityType);
+    $result = $pipeline->getProcess();
+    $this->assertArrayEquals($expected, $result);
+  }
+
+  /**
+   * Test the getProcess with a plugin configured.
+   */
+  public function testGetProcessWithPlugins() {
+    $values = [
+      'process' => [
+        [
+          'plugin' => $this->randomMachineName(),
+          'source' => $this->randomMachineName(),
+          'value' => $this->randomMachineName(),
+        ],
+      ],
+    ];
+    $pipeline = new MigrationProcessPipeline($values, $this->entityType);
+    $result = $pipeline->getProcess();
+    $this->assertArrayEquals($values['process'], $result);
+  }
+
+  /**
+   * Test the getProcess with a replacement configured.
+   */
+  public function testGetProcessWithReplacements() {
+    // A case with replacements.
+    $token = $this->randomMachineName();
+    $value = $this->randomMachineName();
+    $tokens = [
+      $token => $value,
+    ];
+    $values = [
+      'process' => [
+        [
+          'plugin' => $this->randomMachineName(),
+          'source' => $this->randomMachineName(),
+          'value' => $token,
+        ],
+      ],
+    ];
+    $expected = $values['process'];
+    $expected[0]['value'] = $value;
+
+    $pipeline = new MigrationProcessPipeline($values, $this->entityType);
+    $result = $pipeline->getProcess($tokens);
+    $this->assertArrayEquals($expected, $result);
+  }
+
+  /**
+   * Test the method calculateDependencies.
+   */
+  public function testCalculateDependencies() {
+
+    // No new dependencies.
+    $values = [];
+    $pipeline = new MigrationProcessPipeline($values, $this->entityType);
+    $this->assertEmpty($pipeline->calculateDependencies());
+
+    // Test that new dependencies is added properly.
+    $values = [
+      'module' => $this->randomMachineName(),
+    ];
+
+    $definition = $this->createMock(EntityTypeInterface::class);
+    $definition->expects($this->any())
+      ->method('getProvider')
+      ->willReturn($values['module']);
+    $entity_type_manager = $this->createMock(EntityTypeManager::class);
+    $entity_type_manager->expects($this->any())
+      ->method('getDefinition')
+      ->willReturn($definition);
+    $container = new ContainerBuilder();
+    $container->set('entity_type.manager', $entity_type_manager);
+    \Drupal::setContainer($container);
+
+    $pipeline = new MigrationProcessPipeline($values, $this->entityType);
+    $this->assertCount(1, $pipeline->calculateDependencies());
+  }
+
+}
